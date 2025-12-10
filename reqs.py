f34b24686b18ace8594e2dda590b9eeae2eb3417
CostinR DevOps <osti.rzv@gmail.com>
2025-12-10 23:53:59 +0200

    We made some changes to the script to return specific parameters

--- reqs.py
+++ reqs.py
@@ -1,24 +1,55 @@
-import requests
 import json
-from dotenv import load_dotenv
+import requests
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
+
 api_key = os.getenv("API_KEY")
 
 url = "https://www.googleapis.com/youtube/v3/videos"
 
-params = {
-    "id": "8qioenyx4Go",
-    "key": api_key,
-    "part": "snippet"
-}
+def get_tile_publish(id):
+    params = {
+        "id": str(id),
+        "key": api_key,
+        "part": "snippet"
+    }
 
-response = requests.get(url, params=params)
+    response = requests.get(url, params=params)
+    print(response.status_code)
+    response = response.json()
+    title = response["items"][0]["snippet"]["title"]
+    published = response["items"][0]["snippet"]["publishedAt"]
+    
+    return title, published
 
-data = response.json()
+titlu, uptime = get_tile_publish("8qioenyx4Go")
+
+def get_stats(id):
+
+    params = {
+        "id": str(id),
+        "key": api_key,
+        "part": "statistics"
+    }
+
+    response = requests.get(url, params=params)
+    print(response.status_code)
+    response = response.json()
+
+    views = response["items"][0]["statistics"]["viewCount"]
+    likes = response["items"][0]["statistics"]["likeCount"]
+    comments = response["items"][0]["statistics"]["commentCount"]
+    
+    return views, likes, comments
+
+views, likes, comments = get_stats("8qioenyx4Go")
+
+print(f"The Video: {titlu}\n Publshed on: {uptime} has {views} views, {likes} likes and {comments} comments.")
+
+#print(json.dumps(my_rez, indent=4))
+#print(titlu, uptime)
 #data2 = json.dumps(response.json(), indent=4)
-
-print(response.status_code)
-
-print(data["items"][0]["id"])
+#print(data)
+#print(data["items"][0]["id"])
