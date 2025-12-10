b8c46a56776d62b93d071f70bde97886d7747034
CostinR DevOps <osti.rzv@gmail.com>
2025-12-10 16:50:32 +0200

    Am ascuns api key-ul

--- reqs.py
+++ reqs.py
@@ -1,11 +1,16 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 url = "https://www.googleapis.com/youtube/v3/videos"
 
 params = {
     "id": "8qioenyx4Go",
-    "key": "AIzaSyCnsopNmbexdvZJMo3L_-Znmf0bdgaPTxQ",
+    "key": api_key,
     "part": "snippet"
 }
 
@@ -14,6 +19,6 @@ response = requests.get(url, params=params)
 data = response.json()
 #data2 = json.dumps(response.json(), indent=4)
 
-
+print(response.status_code)
 
 print(data["items"][0]["id"])
